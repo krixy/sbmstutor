a420d5115e8ba9fa485eede02bf579f7d0e55c03
krixy <kszajko@gmail.com>
2020-02-24 21:02:33 +0100

    initial commit

---
+++ src/main/java/com/krixy/springboot/microservice/example/forex/sbmstutor/SbmstutorApplication.java
@@ -0,0 +1,13 @@
+package com.krixy.springboot.microservice.example.forex.sbmstutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmstutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmstutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krixy/springboot/microservice/example/forex/sbmstutor/SbmstutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krixy.springboot.microservice.example.forex.sbmstutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmstutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
